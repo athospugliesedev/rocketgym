27e9bbe8a67136c61f893732d8fa52e3f524f831
athospugliesedev <athospugliesedev@gmail.com>
2023-04-29 23:42:22 -0300

    new pages 1/3

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,45 @@
-import { Center, Text } from "native-base";
+import { VStack, HStack, FlatList, Heading, Text } from "native-base";
+import HomeHeader from "@components/HomeHeader";
+import { Group } from "@components/Group";
+import { useState } from "react";
+import { ExerciseCard } from "@components/ExerciseCard";
 
-export function Home(){
-  return(
-    <Center flex={1}>
-      <Text>Home</Text>
-    </Center>
+export function Home() {
+  const [groupSelected, setGroupSelected] = useState('costas');
+  const [groups, setGroups] = useState(['costas', 'ombros', 'Tríceps', 'Bíceps']);
+
+  return (
+    <VStack flex={1}>
+      <HomeHeader />
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) =>
+        (
+          <Group
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{ px: 8 }}
+        my={10}
+        maxH={10}
+      />
+
+      <VStack flex={1} px={8}>
+        <HStack justifyContent="space-between" mb={5}>
+          <Heading color="gray.200" fontSize="md">
+            Exercícios
+          </Heading>
+          <Text color="gray.200" fontSize="sm">
+            4
+          </Text>
+        </HStack>
+        <ExerciseCard />
+      </VStack>
+    </VStack>
   )
 }
